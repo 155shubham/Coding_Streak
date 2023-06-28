d854fdfc9bb9d9506e217e92683e4c48058b22cb
Shubham <155.shubham@gmail.com>
2023-06-28 14:33:08 +0530

    Included countofelements and reverseinarange

---
+++ DSA_Programs/DSA_Programs/Arrays/1_CountOfElements.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DSA_Programs.Arrays
+{
+    /*
+    Q1.Count of elements
+        Solved
+        feature icon
+        Using hints is now penalty free
+        Use Hint
+            Problem Description
+            Given an array A of N integers.
+        Count the number of elements that have at least 1 elements greater than itself.
+
+
+        Problem Constraints
+        1 <= N <= 105
+        1 <= A[i] <= 109
+
+
+        Input Format
+        First and only argument is an array of integers A.
+
+
+        Output Format
+        Return the count of elements.
+
+
+        Example Input
+        Input 1:
+        A = [3, 1, 2]
+        Input 2:
+        A = [5, 5, 3]
+
+
+        Example Output
+        Output 1:
+        2
+        Output 2:
+        1
+
+
+        Example Explanation
+        Explanation 1:
+        The elements that have at least 1 element greater than itself are 1 and 2
+        Explanation 2:
+        The elements that have at least 1 element greater than itself is 3
+        */
+    internal class CountOfElements
+    {
+        public static int solve(List<int> A)
+        {
+
+            //validate length of N
+            int n = A.Count;
+            if (n < 1 || n > 100000) return 0;
+
+            //validate elements of A
+            for (int i = 0; i < n; i++)
+            {
+                if (A[i] < 1 || A[i] > 1000000000) return 0;
+            }
+
+            // Find the max
+            int max = A[0];    // Assume max = first element of A   
+            for (int i = 1; i < n; i++)
+            {
+                if (A[i] > max)
+                {
+                    max = A[i];
+                }
+            }
+
+            // Find the number of elements less than the max 
+            int cnt = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (A[i] < max)
+                {
+                    cnt++;
+                }
+            }
+
+            return cnt;
+
+        }
+    }
+}
+
+
+//TC: O(n) : n + n 
+// SC: O(1) : All constants terms like n , i. max, cnt;
---
+++ DSA_Programs/DSA_Programs/Arrays/3_ReverseInARange.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Programs.Arrays
+{
+
+    /*
+    Q3. Reverse in a range
+    Given an array A of N integers and also given two integers B and C. Reverse the elements of the array A within the given inclusive range [B, C].
+
+
+    Problem Constraints
+    1 <= N <= 105
+    1 <= A[i] <= 109
+    0 <= B <= C <= N - 1
+
+
+    Input Format
+    The first argument A is an array of integer.
+    The second and third arguments are integers B and C
+
+
+    Output Format
+    Return the array A after reversing in the given range.
+
+
+    Example Input
+    Input 1:
+
+    A = [1, 2, 3, 4]
+    B = 2
+    C = 3
+    Input 2:
+
+    A = [2, 5, 6]
+    B = 0
+    C = 2
+
+
+    Example Output
+    Output 1:
+
+    [1, 2, 4, 3]
+    Output 2:
+
+    [6, 5, 2]
+
+
+    Example Explanation
+    Explanation 1:
+
+    We reverse the subarray [3, 4].
+    Explanation 2:
+
+    We reverse the entire array [2, 5, 6].
+    */
+    internal class ReverseInARange
+    {
+        public static List<int> solve_for_specific_range(List<int> A, int B, int C)
+        {
+            int i = B;
+            int j = C;
+            int temp = 0;
+
+            while (i <= j)
+            {
+                temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+                i++;
+                j--;
+            }
+
+            return A;
+        }
+
+
+        //the following is generic program b = 0 and c = n - 1
+        public static List<int> solve_for_all(List<int> A)
+        {
+            int n = A.Count;
+            int i = 0;
+            int j = n - 1;
+            int temp = 0;
+
+            while (i < j)
+            {
+                temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+                i++; ;
+                j--; ;
+            }
+
+            return A;
+        }
+    }
+}
+
+
+// TC: O(N)
+// SC: O(1)
