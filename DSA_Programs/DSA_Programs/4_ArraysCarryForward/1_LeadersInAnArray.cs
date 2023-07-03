97938dc7c317a91f20ee12c01b8b64685c2a9256
Shubham <155.shubham@gmail.com>
2023-07-03 14:03:17 +0530

    Arraycarryforward first program and class files

---
+++ DSA_Programs/DSA_Programs/4_ArraysCarryForward/1_LeadersInAnArray.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Programs.ArraysCarryForward
+{
+
+    /*
+    Q2. Leaders in an array
+
+    Problem Description
+    Given an integer array A containing N distinct integers, you have to find all the leaders in array A. An element is a leader if it is strictly greater than all the elements to its right side.
+
+    NOTE: The rightmost element is always a leader.
+
+
+    Problem Constraints
+    1 <= N <= 105
+    1 <= A[i] <= 108
+
+
+    Input Format
+    There is a single input argument which a integer array A
+
+
+    Output Format
+    Return an integer array denoting all the leader elements of the array.
+
+
+    Example Input
+    Input 1:
+     A = [16, 17, 4, 3, 5, 2]
+    Input 2:
+     A = [5, 4]
+
+
+    Example Output
+    Output 1:
+    [17, 2, 5]
+    Output 2:
+    [5, 4]
+
+
+    Example Explanation
+    Explanation 1:
+     Element 17 is strictly greater than all the elements on the right side to it.
+     Element 2 is strictly greater than all the elements on the right side to it.
+     Element 5 is strictly greater than all the elements on the right side to it.
+     So we will return these three elements i.e [17, 2, 5], we can also return [2, 5, 17] or [5, 2, 17] or any other ordering.
+    Explanation 2:
+     Element 5 is strictly greater than all the elements on the right side to it.
+     Element 4 is strictly greater than all the elements on the right side to it.
+     So we will return these two elements i.e [5, 4], we can also any other ordering.
+    */
+    internal class LeadersInAnArray
+    {
+        public static List<int> solve(List<int> A)
+        {
+            int n = A.Count;
+            List<int> leaderLst = new List<int>();
+
+            if (n < 1 || n > 100000) return leaderLst;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (A[i] < 1 || A[i] > 100000000) return leaderLst;
+            }
+
+            int maxSoFar = A[n - 1];
+            leaderLst.Add(maxSoFar);
+
+            for (int i = n - 2; i > 0; i--)
+            {
+                if (A[i] > maxSoFar)
+                {
+                    leaderLst.Add(A[i]);
+                    maxSoFar = A[i];
+                }
+            }
+
+            return leaderLst;
+        }
+    }
+}
--- DSA_Programs/DSA_Programs/Program.cs
+++ DSA_Programs/DSA_Programs/Program.cs
@@ -4,8 +4,20 @@ using DSA_Programs.ArraysPrefixSum;
 using DSA_Programs.Arrays;
 using DSA_Programs.Interview_Snippets;
 using DSA_Programs.RandomPrograms;
+using DSA_Programs.ArraysCarryForward;
 
 //Console.WriteLine("Welcome to World of Programs");
+
+
+//********************************ArraysCarryForward*****************************************
+
+LeadersInAnArray.solve(new List<int>() {1,2 });
+LeadersInAnArray.solve(new List<int>() { 93, 57, 83, 41, 100, 10, 79, 27, 
+    94, 22, 4, 96, 48, 18, 89, 37, 21, 5, 46, 81, 15, 30, 47, 23, 34, 65, 55, 9, 36, 20, 54, 17, 7, 56, 
+    78, 84, 87, 97, 16, 69, 28, 11, 44, 49, 8, 25, 98, 75, 53, 62, 19, 24, 
+    80, 68, 50, 91, 1, 86, 77, 14, 72, 66, 42, 63, 73, 45, 31, 61, 85, 64, 
+    35, 32, 92, 71, 74, 3, 99, 52, 90, 43, 6, 40, 38, 2, 12, 59, 29, 82, 76, 60, 67, 13, 70, 58, 39, 33, 95, 
+    88, 51, 26 });
 
 //********************************3_ArraysPrefixSum*****************************************
 
