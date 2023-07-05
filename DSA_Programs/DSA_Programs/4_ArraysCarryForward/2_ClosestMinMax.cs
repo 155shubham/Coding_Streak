4590cd94c9b93dada0ac039e328dbddc6717bbc8
Shubham <155.shubham@gmail.com>
2023-07-05 22:32:15 +0530

    Added SpecialSubSequence and ClosestMinMax

---
+++ DSA_Programs/DSA_Programs/4_ArraysCarryForward/2_ClosestMinMax.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Programs.ArraysCarryForward
+{
+
+    /*
+    
+    Q3. Closest MinMax
+
+    Problem Description
+    Given an array A, find the size of the smallest subarray such that it contains at least one occurrence of the maximum value of the array
+
+    and at least one occurrence of the minimum value of the array.
+
+
+
+    Problem Constraints
+    1 <= |A| <= 2000
+
+
+
+    Input Format
+    First and only argument is vector A
+
+
+
+    Output Format
+    Return the length of the smallest subarray which has at least one occurrence of minimum and maximum element of the array
+
+
+
+    Example Input
+    Input 1:
+
+    A = [1, 3, 2]
+    Input 2:
+
+    A = [2, 6, 1, 6, 9]
+
+
+    Example Output
+    Output 1:
+
+     2
+    Output 2:
+
+     3
+
+
+    Example Explanation
+    Explanation 1:
+
+     Take the 1st and 2nd elements as they are the minimum and maximum elements respectievly.
+    Explanation 2:
+
+     Take the last 3 elements of the array.
+    */
+    internal class ClosestMinMax
+    {
+        public static int solve(List<int> A)
+        {
+            int n = A.Count;
+
+            // min and min
+            int min = A[0];
+            int max = A[0];
+            for (int i = 1; i < n; i++)
+            {
+                if (A[i] < min) min = A[i];
+                if (A[i] > max) max = A[i];
+            }
+
+            int maxIndex = -1;
+            int minIndex = -1;
+            int ans = n; // store the max value greater than the length of the array
+            int temp = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (A[i] == min) minIndex = i;
+                if (A[i] == max) maxIndex = i;
+                if (minIndex > -1 && maxIndex > -1)
+                {
+                    temp = (Math.Max(minIndex, maxIndex) - Math.Min(minIndex, maxIndex)) + 1;
+                    if (temp < ans) ans = temp;
+                }
+
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ DSA_Programs/DSA_Programs/4_ArraysCarryForward/3_SpecialSubsequences.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Programs.ArraysCarryForward
+{
+    /*
+    Q4. Special Subsequences "AG"
+
+    Problem Description
+    You have given a string A having Uppercase English letters.
+
+    You have to find how many times subsequence "AG" is there in the given string.
+
+    NOTE: Return the answer modulo 109 + 7 as the answer can be very large.
+
+
+
+    Problem Constraints
+    1 <= length(A) <= 105
+
+
+
+    Input Format
+    First and only argument is a string A.
+
+
+
+    Output Format
+    Return an integer denoting the answer.
+
+
+
+    Example Input
+    Input 1:
+
+     A = "ABCGAG"
+    Input 2:
+
+     A = "GAB"
+
+
+    Example Output
+    Output 1:
+
+     3
+    Output 2:
+
+     0
+
+
+    Example Explanation
+    Explanation 1:
+
+     Subsequence "AG" is 3 times in given string 
+    Explanation 2:
+
+     There is no subsequence "AG" in the given string.
+    */
+    internal class SpecialSubsequences
+    {
+        public static int solve(string A)
+        {
+            int n = A.Length;
+
+            int ACount = 0;
+            int AIndex = -1;
+            int GIndex = -1;
+            int ans = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (A[i] == 'A')
+                {
+                    ACount++;
+                    AIndex = i;
+                }
+                if (A[i] == 'G')
+                {
+                    // GCount++; 
+                    GIndex = i;
+                }
+                if (AIndex > -1 && GIndex > -1 && GIndex > AIndex && A[i] == 'G') ans = (ans + ACount) % 1000000007; // GUGPU A G A FQBMPY A GG AA O A L A EL GGG A O G L G  E G Z
+            }
+
+            return ans;
+        }
+    }
+}
