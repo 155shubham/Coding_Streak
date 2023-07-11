889ba290c40aca03f5a9cb30ce752bc41b7544ec
Shubham <155.shubham@gmail.com>
2023-07-12 00:49:57 +0530

    Arrays carry forward

---
+++ DSA_Programs/DSA_Programs/4_ArraysCarryForward/4_SumOfAllSubArrays.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Programs.ArraysCarryForward
+{
+    internal class SumOfAllSubArrays
+    {
+        public long subarraySum(List<int> A)
+        {
+            int n = A.Count;
+
+            if (n < 1 || n > 100000) return 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (A[i] < 1 || A[i] > 10000) return 0;
+            }
+
+            long totalSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                totalSum += (long)A[i] * (i + 1) * (n - i);
+            }
+
+            return totalSum;
+        }
+    }
+}
---
+++ DSA_Programs/DSA_Programs/4_ArraysCarryForward/5_GenerateAllSubArrays.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Programs.ArraysCarryForward
+{
+    internal class GenerateAllSubArray
+    {
+        public List<List<int>> solve(List<int> A)
+        {
+            int n = A.Count;
+            List<List<int>> res = new List<List<int>>();
+
+            if (n < 1 || n > 100) return new List<List<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (A[i] < 1 || A[i] > 100000) return new List<List<int>>();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = i; j < n; j++)
+                {
+                    List<int> lst3 = new List<int>();
+                    for (int k = i; k <= j; k++)
+                    {
+                        lst3.Add(A[k]);
+                    }
+                    res.Add(lst3);
+                }
+            }
+
+            return res;
+        }
+    }
+}
