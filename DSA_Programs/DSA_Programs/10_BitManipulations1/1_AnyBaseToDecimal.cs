13a3e9f6194c432495ef4efa5b0d990efb3d53a4
Shubham <155.shubham@gmail.com>
2023-07-19 13:58:44 +0530

    AnyBasetoDecimal

---
+++ DSA_Programs/DSA_Programs/10_BitManipulations1/1_AnyBaseToDecimal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Programs.Bit_Manipulations
+{
+    internal class AnyBaseToDecimal
+    {
+        public static int solve(int A, int B)
+        {
+            string numStr = A.ToString();
+            int len = numStr.Length;
+            int numDNS = 0;
+            int digit = 0;
+
+            for (int i = 0; i < len; i++)
+            {
+                digit = (int)Char.GetNumericValue(numStr[len-1-i]);
+                numDNS += (digit * (int)Math.Pow(B, i));
+            }
+
+            return numDNS;
+        }
+    }
+}
