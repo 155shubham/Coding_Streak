325cb612efb7494bf3864fb53c3b717a65b01762
Shubham <155.shubham@gmail.com>
2023-07-24 23:20:16 +0530

    sortedarraybyleastrepeated

---
+++ DSA_Programs/DSA_Programs/RandomPrograms/SortedArrayByLeastRepeated.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Programs.RandomPrograms
+{
+
+    /*
+    Given an array of integers , sort the array by the least repeated integers,. if multiple integers have the same number occurences, 
+    sort them in descending order. Return the final array. Write the program in c#.
+    */
+    internal class SortedArrayByLeastRepeated
+    {     
+        public static int[] solve(int[] arr)
+        {
+            Dictionary<int, int> occurrenceCount = new Dictionary<int, int>();
+            foreach (int num in arr)
+            {
+                if (occurrenceCount.ContainsKey(num))
+                    occurrenceCount[num]++;
+                else
+                    occurrenceCount[num] = 1;
+            }
+
+            var sorted = arr
+                .OrderBy(x => occurrenceCount[x]) // Sort by least repeated
+                .ToArray();
+
+            return sorted;
+        }
+    }
+}
