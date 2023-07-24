51013175ab3007b02da0bf9cdec96e7e02f2aa8b
Shubham <155.shubham@gmail.com>
2023-07-24 23:19:53 +0530

    Powerwithmodules

---
+++ DSA_Programs/DSA_Programs/11_ModularAritmeticIntroduction/1_PowerWithModules.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Programs.ModularAritmeticIntroduction
+{
+    internal class PowerWithModules
+    {
+        public static int solve(int A, int B, int C)
+        {
+            long ans = 1;
+
+            for (int i = 1; i <= B; i++)
+            {
+                ans = (ans * A) % C;
+            }
+
+            return (int)(ans % C);
+        }
+    }
+}
